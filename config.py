d31d4ddcb6025318762204c2c343508c5caac012
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-01-22 18:29:23 -0500

    [subprocess] add exception.

--- config.py
+++ config.py
@@ -575,15 +575,11 @@ def get_system_stats():
             output, _, _ = get_subprocess_output(['grep', 'model name', '/proc/cpuinfo'], log)
             systemStats['cpuCores'] = len(output.splitlines())
 
-        if Platform.is_darwin(platf):
-            output, _, _ = get_subprocess_output(['sysctl', 'hw.ncpu'], log)
-            systemStats['cpuCores'] = int(output.split(': ')[1])
-
-        if Platform.is_freebsd(platf):
+        if Platform.is_darwin(platf) or Platform.is_freebsd(platf):
             output, _, _ = get_subprocess_output(['sysctl', 'hw.ncpu'], log)
             systemStats['cpuCores'] = int(output.split(': ')[1])
     except Exception as e:
-        log.warning("unable to retrieve number of cpuCores. Failed with error %s" % str(e))
+        log.warning("unable to retrieve number of cpuCores. Failed with error %s", e)
 
     if Platform.is_linux(platf):
         systemStats['nixV'] = platform.dist()
